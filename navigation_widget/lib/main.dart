3eb4bd1a3c86e1feaaaa9e4631b24c60b72427f5
bsy7021 <bsy7021@naver.com>
2025-02-03 11:10:13 +0900

    flutter study

---
+++ navigation_widget/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:navigation_widget/first_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '네비게이션 위젯',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: FirstScreen()
+    );
+  }
+}
